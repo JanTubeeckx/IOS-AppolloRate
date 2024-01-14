a66e4e79497aeee0c02cd6afb4ca269433584801
jantubeeckx <jan.tubeeckx@elmos.be>
2024-01-15 00:16:34 +0100

    Create home page view

--- AppolloRate/AppolloRate/ContentView.swift
+++ AppolloRate/AppolloRate/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Jakke!")
+        VStack (alignment: .center) {
+            HStack {
+                Image("Logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 36.0, height: 36.0)
+                Text("AppolloRate.")
+                    .font(.largeTitle)
+            }.padding(30)
+            
+            MenuCardView(label: "Schaderegistratie")
+            HStack {
+                MenuCardView(label: "Inventarisaties")
+                MenuCardView(label: "AppolloRate.")
+            }
+            Text("De inventarisatietool voor uw bibliotheek")
+                .font(.title2)
+                .multilineTextAlignment(.center)
+                .padding(40)
         }
         .padding()
+    }
+}
+
+struct MenuCardView: View {
+    var label: String
+    let FAIcons = "FontAwesome6Free-Solid"
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 5)
+                .foregroundColor(.blue)
+            VStack {
+                Image("Clipboard")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.white)
+                    .frame(width: 56.0, height: 56.0)
+                Text(label)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+            }
+            
+        }.onTapGesture {
+            print("tapped")
+        }
     }
 }
 

9c3638cd62652ca4df906571909a8fe97f247e58
jantubeeckx <jan.tubeeckx@elmos.be>
2024-01-14 17:38:42 +0100

    Create project

---
+++ AppolloRate/AppolloRate/AppolloRateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppolloRateApp.swift
+//  AppolloRate
+//
+//  Created by Jan Tubeeckx on 14/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppolloRateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppolloRate/AppolloRate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppolloRate
+//
+//  Created by Jan Tubeeckx on 14/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Jakke!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

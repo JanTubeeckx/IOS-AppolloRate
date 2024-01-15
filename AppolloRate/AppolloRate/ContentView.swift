e8ad5a398ed4798fdc41f1437a9b7aa0062aa018
jantubeeckx <jan.tubeeckx@elmos.be>
2024-01-15 22:00:48 +0100

    Finish home page

--- AppolloRate/AppolloRate/ContentView.swift
+++ AppolloRate/AppolloRate/ContentView.swift
@@ -10,63 +10,102 @@ import SwiftUI
 struct HomePageView: View {
     var body: some View {
         VStack (alignment: .center, spacing: 10) {
-            HStack {
-                Image("Logo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 36.0, height: 36.0)
-                Text("AppolloRate.")
-                    .font(.largeTitle)
-            }.padding(40)
-            MenuCardView(label: "Schaderegistratie", image: "Clipboard", isBigCard: true)
-            HStack(spacing: 10) {
-                MenuCardView(label: "Inventarisaties", image: "List")
-                MenuCardView(label: "AppolloRate.", image: "Info")
-            }
-            Text("De inventarisatietool voor uw bibliotheek")
-                .font(.title2)
-                .multilineTextAlignment(.center)
-                .padding(40)
+            logo
+            bigCardButton
+            smallCardButtons
+            slogan
         }
         .padding()
+    }
+    
+    var logo: some View {
+        HStack {
+            Image(systemName: "book.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 36.0, height: 36.0)
+            Text("AppolloRate.")
+                .font(.largeTitle)
+        }.padding(40)
+    }
+    
+    var bigCardButton: some View {
+        MenuCardView(label: "Schaderegistratie", image: "pencil.and.list.clipboard", isBigCard: true)
+    }
+    
+    var smallCardButtons: some View {
+        HStack(spacing: 10) {
+            MenuCardView(label: "Inventarisaties", image: "list.bullet")
+            MenuCardView(label: "AppolloRate.", image: "info.circle")
+        }
+    }
+    
+    var slogan: some View {
+        Text("De inventarisatietool voor uw bibliotheek")
+            .font(.title2)
+            .multilineTextAlignment(.center)
+            .padding(40)
     }
 }
 
 struct MenuCardView: View {
-    var label: String
-    var image: String
-    var isBigCard: Bool = false
+    let label: String
+    let image: String
+    var isBigCard = false
     
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 5)
-                .foregroundColor(.blue)
-            VStack {
-                if isBigCard {
-                    Image(image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 65.0, height: 65.0)
-                        .padding(10)
-                    Text(label)
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                } else {
-                    Image(image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 45.0, height: 45.0)
-                        .padding(10)
-                    Text(label)
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                }
-            }
-        }.onTapGesture {
+            card
+            content
+        }
+        .foregroundColor(.white)
+        .onTapGesture {
             print("tapped")
         }
+    }
+    
+    var card: some View {
+        RoundedRectangle(cornerRadius: 5)
+            .foregroundColor(.blue)
+    }
+    
+    var content: some View {
+        VStack {
+            if isBigCard {
+                bigIcon
+                largeLabel
+            } else {
+                smallIcon
+                smallLabel
+            }
+        }
+    }
+    
+    var bigIcon: some View {
+        Image(systemName: image)
+            .imageScale(.large)
+            .font(.system(size: 44))
+            .padding(10)
+    }
+    
+    var largeLabel: some View {
+        Text(label)
+            .font(.title)
+            .fontWeight(.semibold)
+    }
+    
+    var smallIcon: some View {
+        Image(systemName: image)
+            .imageScale(.large)
+            .font(.system(size: 38))
+            .frame(width: 42.0, height: 42.0)
+            .padding(10)
+    }
+    
+    var smallLabel: some View {
+        Text(label)
+            .font(.title2)
+            .fontWeight(.semibold)
     }
 }
 

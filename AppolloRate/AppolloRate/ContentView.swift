26bb1ea68a028fe5378e61b885f957372adeadb8
jantubeeckx <jan.tubeeckx@elmos.be>
2024-01-15 16:03:11 +0100

    Refactor homepage

--- AppolloRate/AppolloRate/ContentView.swift
+++ AppolloRate/AppolloRate/ContentView.swift
@@ -18,23 +18,7 @@ struct HomePageView: View {
                 Text("AppolloRate.")
                     .font(.largeTitle)
             }.padding(40)
-            ZStack {
-                RoundedRectangle(cornerRadius: 5)
-                    .foregroundColor(.blue)
-                VStack {
-                    Image("Clipboard")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 65.0, height: 65.0)
-                        .padding(10)
-                    Text("Schaderegistratie")
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                }
-            }.onTapGesture {
-                print("tapped")
-            }
+            MenuCardView(label: "Schaderegistratie", image: "Clipboard", isBigCard: true)
             HStack(spacing: 10) {
                 MenuCardView(label: "Inventarisaties", image: "List")
                 MenuCardView(label: "AppolloRate.", image: "Info")
@@ -51,21 +35,34 @@ struct HomePageView: View {
 struct MenuCardView: View {
     var label: String
     var image: String
+    var isBigCard: Bool = false
     
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 5)
                 .foregroundColor(.blue)
             VStack {
-                Image(image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 45.0, height: 45.0)
-                    .padding(10)
-                Text(label)
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .foregroundColor(.white)
+                if isBigCard {
+                    Image(image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 65.0, height: 65.0)
+                        .padding(10)
+                    Text(label)
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                } else {
+                    Image(image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 45.0, height: 45.0)
+                        .padding(10)
+                    Text(label)
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                }
             }
         }.onTapGesture {
             print("tapped")
